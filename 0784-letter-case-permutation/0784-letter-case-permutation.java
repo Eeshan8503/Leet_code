90bf1dc1f22b04e4d3194b6a1054760d65001601
Eeshan Mattey <71863350+Eeshan8503@users.noreply.github.com>
2023-03-31 20:20:17 +0530

    Time: 10 ms (21.87%), Space: 43.3 MB (41.29%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+         char[] arr =s.toCharArray();
+         List<String> ans=new ArrayList<>();
+        HashSet<String> set=new HashSet<>();
+        func(ans,0,arr,set);
+        return ans;
+    }
+   public void func(List<String> ans, int count, char[] s,HashSet<String> set){
+//        System.out.println("count is "+count);
+        if(set.contains(String.valueOf(s))){
+            return;
+        }
+        if(s.length==count){
+//            System.out.println("here");
+            ans.add(String.valueOf(s));
+            set.add(String.valueOf(s));
+//            System.out.println(String.valueOf(s));
+            return;
+        }
+        if(Character.isDigit(s[count])){
+//            System.out.println("Digit encounterd "+s[count]);
+            func(ans,count+1,s,set);
+            return;
+        }
+        for(int i=count;i<s.length;i++){
+           func(ans,count+1,s,set);
+            inverter(s,i);
+            func(ans,count+1,s,set);
+            // inverter(s,i);
+
+        }
+    }
+    public void inverter(char arr[],int i){
+        if(Character.isUpperCase(arr[i])){
+            arr[i]=Character.toLowerCase(arr[i]);
+        }
+        else{
+            arr[i]=Character.toUpperCase(arr[i]);
+        }
+    }
+}
