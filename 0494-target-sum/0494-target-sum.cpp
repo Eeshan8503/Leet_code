118b9e209dfbf5c446d928638082dff5c6d1dc15
Eeshan Mattey <71863350+Eeshan8503@users.noreply.github.com>
2023-04-17 14:39:50 +0530

    Time: 415 ms (32.19%), Space: 9.1 MB (85.29%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    int sol(int i,vector<int>& nums,int target2)
+    {
+       // if(target2==0)
+       //     return 1;
+        
+        if(i==0)
+        {
+            if(target2==0 && nums[i]==0)
+                return 2;
+            
+            if(target2==0 || nums[i]==target2)
+                return 1;
+            
+            else return 0;
+                
+            
+        }
+        
+        int notTake=sol(i-1,nums,target2);
+        
+        int take=0;
+        if(target2>=nums[i])
+            take=sol(i-1,nums,target2-nums[i]);
+        
+        return take+notTake;
+        
+        
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+            sum+=nums[i];
+        
+        if((sum+target)%2!=0)
+            return 0;
+        
+        int target2=(sum+target)/2;
+        
+        return sol(nums.size()-1,nums,target2);
+        
+    }
+};
